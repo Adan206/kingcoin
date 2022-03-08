97aaecbf9fe243f3469d2acc1a9628966557e3b5
Adan Abbi <adanweli@hotmail.com>
2022-03-07 19:46:11 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import { makeStyles } from "@material-ui/core";
+import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
+import Coinpage from "./pages/Coinpage";
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className='App'>
-      <h5>The Truth</h5>
+    <div className={classes.App}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+          <Route path='/coins/:id' element={<Coinpage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
